5bafacb9e944e3cb71579ddafeba5c7db2db2253
devbaoo_712 <caokhacbao0712@gmail.com>
2024-06-08 05:23:04 +0700

    update contact

--- src/components/contact/AddressSidebar.jsx
+++ src/components/contact/AddressSidebar.jsx
@@ -5,15 +5,13 @@ const AddressSidebar = () => {
     <div className="contact_localtion">
       <h4>Contact Us</h4>
       <p>
-      We are here to help! If you have any questions, 
-      concerns, or need assistance, please feel free 
-      to reach out to us through one of the following methods
+        We are here to help! If you have any questions, concerns, or need
+        assistance, please feel free to reach out to us through one of the
+        following methods
       </p>
       <div className="content_list">
         <h5>Address</h5>
-        <p>
-          FPT University HCMC
-        </p>
+        <p>FPT University HCMC</p>
       </div>
       <div className="content_list">
         <h5>Phone</h5>
@@ -21,7 +19,7 @@ const AddressSidebar = () => {
       </div>
       <div className="content_list">
         <h5>Mail</h5>
-        <p>example@fpt.edu.vn</p>
+        <p>baockse172296@fpt.edu.vn</p>
       </div>
       <h5>Follow Us</h5>
       <ul className="contact_form_social_area">
