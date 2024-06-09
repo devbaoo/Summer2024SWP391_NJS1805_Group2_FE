ccea04821e94b37ae7e90198d55e187c3c0a993f
devbaoo_712 <caokhacbao0712@gmail.com>
2024-06-10 03:09:22 +0700

    call api login + logout

--- src/components/common/footer/CopyrightFooter.jsx
+++ src/components/common/footer/CopyrightFooter.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const CopyrightFooter = () => {
   const menuItems = [
     { id: 1, name: "Home", routeLink: "/" },
-    // { id: 2, name: "Listing", routeLink: "/listing-grid-v3" },
+    { id: 2, name: "Listing", routeLink: "/listing-grid-v6" },
     // { id: 3, name: "Property", routeLink: "/listing-grid-v4" },
     // { id: 4, name: "About Us", routeLink: "/about-us" },
-    // { id: 5, name: "Blog", routeLink: "/blog-list-3" },
+    { id: 5, name: "Blog", routeLink: "/blog-list-3" },
     { id: 6, name: "Contact", routeLink: "/contact" },
   ];
 
@@ -28,7 +28,7 @@ const CopyrightFooter = () => {
       <div className="col-lg-6 col-xl-6">
         <div className="copyright-widget text-end">
           <p>
-            &copy; {new Date().getFullYear()} by{" Group 5 (SWP301 - NJS1805) "}
+            &copy; {new Date().getFullYear()} by{" Group 2 (SWP301 - NJS1805) "}
             . All rights reserved.
           </p>
         </div>
--- src/components/common/header/dashboard/MyAccount.jsx
+++ src/components/common/header/dashboard/MyAccount.jsx
@@ -1,48 +1,72 @@
-
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { isSinglePageActive } from "../../../../utils/daynamicNavigation";
 
 const MyAccount = () => {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const fullname = userInfo ? userInfo.fullname : "";
   const profileMenuItems = [
     { id: 1, name: "My Profile", ruterPath: "/my-profile" },
-    { id: 2, name: " My Message", ruterPath: "/my-message" },
-    { id: 3, name: " My Favourite", ruterPath: "/my-favourites" },
-    { id: 4, name: " My Package", ruterPath: "/my-package" },
-    { id: 5, name: " Log out", ruterPath: "/login" },
+    { id: 2, name: "My Message", ruterPath: "/my-message" },
+    { id: 3, name: "My Favourite", ruterPath: "/my-favourites" },
+    { id: 4, name: "My Package", ruterPath: "/my-package" },
+    { id: 5, name: "Log out", ruterPath: "/logout" },
   ];
+
+  const handleLogout = () => {
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("isLoggedIn");
+    navigate("/login");
+  };
+
+  const linkStyle = {
+    default: { color: "#484848" },
+    hover: { color: "#484848" },
+    active: { color: "#ff5a5f" },
+  };
 
   return (
     <>
       <div className="user_set_header">
         <img
-         
           className="float-start"
           src="/assets/images/team/e1.png"
           alt="e1.png"
         />
         <p>
-          Phát Nguyễn <br />
-          <span className="address">admin@mommilk.com</span>
+          {fullname} <br />
+          <span className="address">{userInfo ? userInfo.email : ""}</span>
         </p>
       </div>
-      {/* End user_set_header */}
 
       <div className="user_setting_content">
-        {profileMenuItems.map((item) => (
-          <Link
-            to={item.ruterPath}
-            key={item.id}
-            className="dropdown-item"
-            style={
-              isSinglePageActive(`${item.ruterPath}`, pathname)
-                ? { color: "#ff5a5f" }
-                : undefined
-            }
-          >
-            {item.name}
-          </Link>
-        ))}
+        {profileMenuItems.map((item) =>
+          item.ruterPath === "/logout" ? (
+            <a
+              href="#"
+              key={item.id}
+              className="dropdown-item"
+              onClick={handleLogout}
+              style={linkStyle.default}
+            >
+              {item.name}
+            </a>
+          ) : (
+            <Link
+              to={item.ruterPath}
+              key={item.id}
+              className="dropdown-item"
+              style={
+                isSinglePageActive(item.ruterPath, pathname)
+                  ? linkStyle.active
+                  : linkStyle.default
+              }
+            >
+              {item.name}
+            </Link>
+          )
+        )}
       </div>
     </>
   );
