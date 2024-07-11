dec3dcfb5e0bae08c4d356755f457622c5c143e6
devbaoo_712 <caokhacbao0712@gmail.com>
2024-07-11 07:22:24 +0700

    update UI + order details + admin dashboard

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
 
                     <div className="flex items-center lg:order-2">
                         {!account ? (
-                            <a href="/login" className="border bg-pink-500 text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
+                            <a href="/login" className="border bg-pink-500 text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 font-bold">Log in</a>
                         ) : (
                             <button
                                 onClick={handleLogout}
